9e109d75d078b941feb05223c42ca8229ba71163
Rechild <1102530068@qq.com>
2017-09-17 16:17:56 +0800

    fix chinese encoding bug

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+#coding=utf-8
 #user/bin/python3.6
 import requests
 from bs4 import BeautifulSoup
 import bs4
 import re
+import io
+import sys
+#改变标准输出的默认编码
+sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
 
 def creatFile(file):
     fp = open(file,"w")
@@ -28,18 +33,16 @@ def fillUnivList(ulist, html):
             ulist.append([tds[0].string, tds[1].string, tds[2].string])
 
 def printUnivList(ulist, num):
-    print("{:^10}\t{:^6}\t{:^10}".format("排名", "学校","省份"))
+    print("{:^10}\t{:^6}\t{:^10}".format(u"排名", u"学校",u"省份"))
     for i in range(num):
-        u=ulist[i]
-        # print("{:^10}\t{:^6}\t{:^10}".format(u[0], u[1], u[2]))
-        print(str(u))
+        item=ulist[i]
+        print("{:^10}\t{:^10}\t{:^10}".format(item[0], item[1], item[2]))
 if __name__ == "__main__":
     # url="https://www.lagou.com"
     url="http://www.zuihaodaxue.com/zuihaodaxuepaiming2016.html"
     index_file = "index.txt"
     uinfo = []
-    file = creatFile(index_file)
+    # file = creatFile(index_file)
     html = getHTMLText(url)
     fillUnivList(uinfo, html)
     printUnivList(uinfo, 20)
-    
